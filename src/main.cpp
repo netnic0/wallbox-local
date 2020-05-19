60651b102a7f53f3e15564f79b5af405afc20d36
hobbe <obagot@gmail.com>
2020-05-19 22:39:37 +0200

    rename OCPP functions

--- src/main.cpp
+++ src/main.cpp
@@ -373,7 +373,7 @@ static void send_ocpp_meter_values() {
   send_ocpp_request(ws_connection, OCPP_REQUEST_METER_VALUES, default_uuid, content);
 }
 
-static mg_str stopTransaction(const char *reason) {
+static mg_str ocpp_stop_transaction(const char *reason) {
   LOG(LL_DEBUG,
       ("Stop transaction %d for tag %s, reason %s",
        mgos_sys_config_get_ocpp_transaction_id(),
@@ -410,11 +410,11 @@ static mg_str stopTransaction(const char *reason) {
   return mg_mk_str(OCPP_RESPONSE_ACCEPTED);
 }
 
-static mg_str stopTransaction(const char *payload, const char *reason) {
+static mg_str ocpp_stop_transaction(const char *payload, const char *reason) {
   int id = 0;
   if (json_scanf(payload, strlen(payload), "{ transactionId:%d }", &id) > 0) {
     if (id == mgos_sys_config_get_ocpp_transaction_id()) {
-      return stopTransaction(reason);
+      return ocpp_stop_transaction(reason);
     } else {
       LOG(LL_ERROR,
           ("Payload %s not matching current transaction id %d", payload, mgos_sys_config_get_ocpp_transaction_id()));
@@ -426,7 +426,7 @@ static mg_str stopTransaction(const char *payload, const char *reason) {
   }
 }
 
-static mg_str startTransaction(const char *payload) {
+static mg_str ocpp_start_transaction(const char *payload) {
   if (json_scanf(payload, strlen(payload), "{ idTag:%Q }", &tag_id) > 0) {
     LOG(LL_INFO, ("Starting transaction for tag with id %s", tag_id));
 
@@ -455,17 +455,11 @@ static mg_str startTransaction(const char *payload) {
   }
 }
 
-/*
- * Soft: Return to initial status, gracefully terminating any transactions in progress.
- * At receipt of a soft reset, the Charge Point SHALL return to a state that behaves as just having been booted.
- * If any transaction is in progress it SHALL be terminated normally, before the reset, as in Stop Transaction.
- * Send StatusNotification/ResetFailure if not able to reset.
- */
-static mg_str reset_soft() {
+static mg_str ocpp_reset_soft() {
   LOG(LL_INFO, ("Performing soft reset"));
 
   if (mgos_sys_config_get_ocpp_transaction_id() > 0) {
-    stopTransaction(OCPP_STOP_TRANSACTION_REASON_SOFTRESET);
+    ocpp_stop_transaction(OCPP_STOP_TRANSACTION_REASON_SOFTRESET);
   }
 
   mqtt_announced = false;
@@ -473,17 +467,11 @@ static mg_str reset_soft() {
   return mg_mk_str(OCPP_RESPONSE_ACCEPTED);
 }
 
-/*
- * Hard: Full reboot of Charge Point software.
- * At receipt of a hard reset the Charge Point SHALL attempt to terminate any transaction in progress normally as
- * in StopTransaction and then perform a reboot.
- * Send StatusNotification/ResetFailure if not able to reset.
- */
-static mg_str reset_hard() {
+static mg_str ocpp_reset_hard() {
   LOG(LL_INFO, ("Performing hard reset"));
 
   if (mgos_sys_config_get_ocpp_transaction_id() > 0) {
-    stopTransaction(OCPP_STOP_TRANSACTION_REASON_HARDRESET);
+    ocpp_stop_transaction(OCPP_STOP_TRANSACTION_REASON_HARDRESET);
   }
 
   mgos_system_restart_after(10000);
@@ -491,16 +479,16 @@ static mg_str reset_hard() {
   return mg_mk_str(OCPP_RESPONSE_ACCEPTED);
 }
 
-static mg_str reset(const char *payload) {
+static mg_str ocpp_reset(const char *payload) {
   char *reset_type = NULL;
 
   if (json_scanf(payload, strlen(payload), "{ type:%Q }", &reset_type) > 0) {
     LOG(LL_INFO, ("Resetting in mode %s", reset_type));
 
     if (strcmp(OCPP_RESET_TYPE_SOFT, reset_type) == 0) {
-      return reset_soft();
+      return ocpp_reset_soft();
     } else if (strcmp(OCPP_RESET_TYPE_HARD, reset_type) == 0) {
-      return reset_hard();
+      return ocpp_reset_hard();
     } else {
       LOG(LL_WARN, ("Reset type %s not supported", reset_type));
       return mg_mk_str(OCPP_RESPONSE_REJECTED);
@@ -511,7 +499,7 @@ static mg_str reset(const char *payload) {
   return mg_mk_str(OCPP_RESPONSE_REJECTED);
 }
 
-static mg_str getConfiguration(const char *payload) {
+static mg_str ocpp_get_configuration(const char *payload) {
   char buf[1800];
   int length;
   LOG(LL_DEBUG, ("OCPP GetConfiguration request: %s", payload));
@@ -519,7 +507,7 @@ static mg_str getConfiguration(const char *payload) {
   return mg_mk_str_n(buf, length);
 }
 
-static mg_str changeConfiguration(const char *payload) {
+static mg_str ocpp_change_configuration(const char *payload) {
   LOG(LL_DEBUG, ("OCPP ChangeConfiguration request: %s", payload));
   char *key = NULL;
 
@@ -548,7 +536,7 @@ static mg_str changeConfiguration(const char *payload) {
   return mg_mk_str(OCPP_RESPONSE_REJECTED);
 }
 
-static mg_str updateFirmware(const char *payload) {
+static mg_str ocpp_update_firmware(const char *payload) {
   char *location = NULL;
 
   if (json_scanf(payload, strlen(payload), "{ location:%Q }", &location) > 0) {
@@ -606,17 +594,17 @@ static void handle_ocpp_cmd(struct mg_connection *nc, const char *cmd, const cha
   LOG(LL_DEBUG, ("Handle ocpp cmd %s with id %s", cmd, id));
   struct mg_str data;
   if (strcmp(cmd, OCPP_REQUEST_GET_CONFIGURATION) == 0) {
-    data = getConfiguration(payload);
+    data = ocpp_get_configuration(payload);
   } else if (strcmp(cmd, OCPP_REQUEST_CHANGE_CONFIGURATION) == 0) {
-    data = changeConfiguration(payload);
+    data = ocpp_change_configuration(payload);
   } else if (strcmp(cmd, OCPP_REQUEST_REMOTE_START_TRANSACTION) == 0) {
-    data = startTransaction(payload);
+    data = ocpp_start_transaction(payload);
   } else if (strcmp(cmd, OCPP_REQUEST_REMOTE_STOP_TRANSACTION) == 0) {
-    data = stopTransaction(payload, OCPP_STOP_TRANSACTION_REASON_REMOTE);
+    data = ocpp_stop_transaction(payload, OCPP_STOP_TRANSACTION_REASON_REMOTE);
   } else if (strcmp(cmd, OCPP_REQUEST_RESET) == 0) {
-    data = reset(payload);
+    data = ocpp_reset(payload);
   } else if (strcmp(cmd, OCPP_REQUEST_UPDATE_FIRMWARE) == 0) {
-    data = updateFirmware(payload);
+    data = ocpp_update_firmware(payload);
   } else if (strcmp(cmd, OCPP_REQUEST_CLEAR_CACHE) == 0) {
     data = mg_mk_str(OCPP_RESPONSE_REJECTED);
   } else if (strcmp(cmd, OCPP_REQUEST_UNLOCK_CONNECTOR) == 0) {
@@ -751,7 +739,7 @@ static void wallbox_reboot_handler(struct mg_rpc_request_info *ri,
   LOG(LL_INFO, ("RPC request to reboot"));
 
   // OCPP reset and reboot
-  reset_hard();
+  ocpp_reset_hard();
 
   mg_rpc_send_responsef(ri, "{}");
   (void) cb_arg;
@@ -766,7 +754,7 @@ static void wallbox_reset_handler(struct mg_rpc_request_info *ri,
   LOG(LL_INFO, ("RPC request to reset to factory settings"));
 
   // OCPP reset and reboot
-  reset_hard();
+  ocpp_reset_hard();
 
   // Reset config
   mgos_config_reset(MGOS_CONFIG_LEVEL_USER);
