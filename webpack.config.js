c17a2edb0d14c7804210cd256468532e186a3e7a
Sébastien Savalle <sebastien.savalle@gmail.com>
2021-04-30 08:45:40 +0200

    Webpack5 migration (#85)
    * migrate to webpack 5
    
    * restore clean webpack plugin
    
    * fix script loading
    
    * add babel core
    
    * Update webpack.config.js
    
    Co-authored-by: Olivier Bagot <49272720+olivierbagot@users.noreply.github.com>
    
    Co-authored-by: Sébastien Savalle <>
    Co-authored-by: Olivier Bagot <49272720+olivierbagot@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const HTMLInlineCSSPlugin = require("html-inline-css-webpack-plugin").default;
+const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './www/app/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.min.js'
+        filename: 'bundle.min.js',
+        clean: true
     },
     module: {
         rules: [
@@ -25,38 +26,38 @@ module.exports = {
             },
             {
                 test: /\.css$/u,
-                loaders: [MiniCssExtractPlugin.loader, 'css-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             }
         ]
     },
     optimization: {
         minimize: true,
         minimizer: [
-            new TerserPlugin({
-                parallel: true,
-                test: /\.js(?<x>\?.*)?$/iu,
-            }, new OptimizeCSSAssetsPlugin({})),
+            `...`,
+            new CssMinimizerPlugin(),
         ],
     },
     plugins: [
         new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            inlineSource: '.(js|css)$', // embed all javascript and css inline,
-            template: 'www/index.html'
-        }),
-        new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-        }),
-        new OptimizeCSSAssetsPlugin({}),
-        new CompressionPlugin(),
         new CopyPlugin({
             patterns: [
                 { from: 'www/lib', to: 'gzip' },
                 { from: 'www/lib', to: '' }
             ],
-        })
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: 'www/index.html',
+            inject: 'body',
+        }),
+        new HTMLInlineCSSPlugin(),
+        new HtmlInlineScriptPlugin([
+            /.+[.]js$/u
+          ]),
+        new CompressionPlugin(),
     ],
     devServer: {
         port: 3000,
